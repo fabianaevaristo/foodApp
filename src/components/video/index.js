bc09fe29beb0ffc2332f3e11ca19c7c505c68092
Fabiana Pontes <fabianapontesevaristo@gmail.com>
2023-05-03 20:58:09 -0300

    initial commit

---
+++ src/components/video/index.js
@@ -0,0 +1,43 @@
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native'
+
+import { Feather } from '@expo/vector-icons'
+import { WebView } from 'react-native-webview'
+
+
+export function VideoView( {handLeClose, videoUrL} ){
+  return(
+    <SafeAreaView style={styles.container}>
+       <TouchableOpacity style={styles.backButton} onPress={handLeClose}>
+          <Feather name="arrow-left" size={24} color="#FFF"/>
+          <Text  style={styles.backText}>Voltar</Text>
+       </TouchableOpacity>
+
+       <WebView
+        style={styles.contentView}
+        source={{ uri: videoUrL }}
+       />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+   container:{
+    flex: 1,
+    width: '100%'
+   },
+   backButton:{
+    width: '100%',
+    backgroundColor: '#4CBE6C',
+    height: 48,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingStart: 14,
+   },
+   backText:{
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 500,
+    marginLeft: 14,
+
+   }
+})
